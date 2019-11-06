e863a9338fa7c5c3aacd4dcb25e97b26f79549ac
tanmay <tanmaymandal943@gmail.com>
2019-11-07 01:58:32 +0530

    Fixed password widget

--- sw_users/forms.py
+++ sw_users/forms.py
@@ -2,4 +2,11 @@ from django import forms
 
 class UserForm(forms.Form):
     username = forms.CharField(label='username', max_length=100)
-    password = forms.CharField(label='password', max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class NewUserForm(forms.Form):
+    username = forms.CharField(label='username', max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.EmailField(label='email')
+    name = forms.CharField(label='name', max_length=100)
