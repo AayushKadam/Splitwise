f19dbb4cdf870fbf27e336f922f6228c27e77366
Tanmaymandal943 <tanmaymandal943@gmail.com>
2019-11-24 09:53:29 +0530

    small change in database layout

--- sgroups/models.py
+++ sgroups/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Groups(models.Model):
+	def __str__ (self):
+		return self.name
+	name = models.CharField(max_length=100)
+
+class Groupfriend(models.Model):
+	def __str__(self):
+		return self.friend1.username + " " + self.friend2.username
+	groups = models.ForeignKey(Groups,on_delete=models.CASCADE,related_name='groups')
+	friend1 = models.ForeignKey(User,on_delete=models.CASCADE,related_name='gfriend1')
+	friend2 = models.ForeignKey(User,on_delete=models.CASCADE,related_name='gfriend2')
+	# friend2 gives money to friend1
+	money = models.FloatField(default=0.0)
+
+class Groupmoney(models.Model):
+	def __str__(self):
+		return self.friend.username
+	groups = models.ForeignKey(Groups,on_delete=models.CASCADE,related_name='ggroups')
+	friend = models.ForeignKey(User,on_delete=models.CASCADE,related_name='gfriend')
+	# friend2 gives money to friend1
+	money = models.FloatField(default=0.0)
